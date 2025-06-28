1af90386e004115b7e2610cdfac023eb1830a9be
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-06-27 21:35:26 -0300

    v0.4.8 new UI and settings BETA

--- future/web/test/rectangle-handlers.test.js
+++ future/web/test/rectangle-handlers.test.js
@@ -11,19 +11,24 @@ describe('rectangle-handlers', () => {
   beforeEach(() => {
     document.body.innerHTML = `
       <div id="splashScreen">
-        <button id="powerOn">⏻</button>
+        <button id="powerOn">Power On (⏻)</button>
       </div>
       <div id="mainContainer">
-        <button id="button1">Start</button>
+        <button id="button1" class="video-container">
+          <video id="videoFeed"></video>
+          <span class="button-text">Start</span>
+        </button>
         <button id="button2">Audio On</button>
         <button id="button3">30 FPS</button>
         <button id="button4">Save Settings</button>
         <button id="button5">Load Settings</button>
-        <button id="button6" class="video-container">
-          <video id="videoFeed"></video>
-          <span class="button-text">Settings</span>
-        </button>
+        <button id="button6">Settings</button>
         <div id="loadingIndicator">Loading...</div>
+      </div>
+      <div id="debug" class="debug-panel">
+        <pre></pre>
+        <button id="emailDebug">Email Log</button>
+        <button id="closeDebug">Close</button>
       </div>
     `;
   });
