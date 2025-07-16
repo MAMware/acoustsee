776e7b117f684acae0254a26552c8d55ff4c5529
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-07-15 23:45:47 -0300

    v0.5.2
    Removed setupStreamControl import and call

--- future/web/ui/ui-controller.js
+++ future/web/ui/ui-controller.js
@@ -1,13 +1,11 @@
 // future/web/ui-controller.js
 import { setupAudioControls } from './audio-controls.js';
-import { setupStreamControl } from './stream-control.js';
 import { setupUISettings } from './ui-settings.js';
 import { setupCleanupManager } from './cleanup-manager.js';
 
 export function setupUIController({ dispatchEvent, DOM }) {
   console.log('setupUIController: Starting setup');
   setupAudioControls({ dispatchEvent, DOM });
-  setupStreamControl({ dispatchEvent, DOM });
   setupUISettings({ dispatchEvent, DOM });
   setupCleanupManager();
   console.log('setupUIController: Setup complete');
