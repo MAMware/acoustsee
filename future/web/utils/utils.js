80cf18b6b3668d8a8bef1b2a4f9484cebdb556c8
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-08-01 12:29:32 +0000

    fixed translation dir error

--- future/web/utils/utils.js
+++ future/web/utils/utils.js
@@ -28,7 +28,7 @@ export async function getText(key, params = {}, type = 'tts') {
 
     let translations = translationsCache[language.id];
     if (!translations) {
-      const response = await fetch(`./languages/${language.id}.json`);
+      const response = await fetch(`../languages/${language.id}.json`);
       if (!response.ok) throw new Error(`Failed to load language file: ${response.status}`);
       translations = await response.json();
       translationsCache[language.id] = translations;
