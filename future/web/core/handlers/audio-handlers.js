ba1040874c537bba40212b88553e9b553cb41224
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-08-13 05:00:34 +0000

    fix: update import path for available synths from JSON to JS file

--- future/web/core/handlers/audio-handlers.js
+++ future/web/core/handlers/audio-handlers.js
@@ -1,7 +1,7 @@
 // web/core/handlers/audio-handlers.js
 
 import { structuredLog } from '../../utils/logging.js';
-import availableEngines from '../../audio/synths/available-engines.json';
+import availableEngines from '../../audio/synths/available-synths.js';
 import { resizeOscillatorPool, initializeAudio } from '../../audio/audio-processor.js';
 import { settings } from '../state.js';
 import { applyHRTF as hrtfSpatialize } from '../../audio/hrtf-processor.js';
