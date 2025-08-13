6eb9d585e7204996efdef89bc40add3676e3e8e9
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-08-13 00:29:14 +0000

    Checkpoint from VS Code for coding agent session

---
+++ future/web/video/grids/available-grids.js
@@ -0,0 +1,7 @@
+import { mapFrameToCircleOfFifths } from './circle-of-fifths.js';
+import { mapFrameToHexTonnetz } from './hex-tonnetz.js';
+
+export const availableGrids = [
+   { id: 'circle-of-fifths', mapFunction: mapFrameToCircleOfFifths },
+  { id: 'hex-tonnetz', mapFunction: mapFrameToHexTonnetz }
+];
