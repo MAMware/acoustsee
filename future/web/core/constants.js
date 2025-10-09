a530b069282b1d7db69276c0670375c700f47c8a
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-10-09 19:48:33 +0000

    Updates AUDIO_VERSION constant

--- future/web/core/constants.js
+++ future/web/core/constants.js
@@ -12,7 +12,7 @@ export const LOG_LEVELS = {
 
 // Version constants for different modules
 export const BUILD_VERSION = '0.8.8-simplerCues';
-export const AUDIO_VERSION = '0.8.2-simplerCues';
+export const AUDIO_VERSION = '0.8.2-simplerCues!';
 export const VIDEO_VERSION = '0.6-lucasKanadeFlow';
 export const UI_VERSION = '0.6.4-devHTML';
 export const LANGUAGES_VERSION = '0.2-spaEng';
