42a46f60f3f58077c2a2fff4b5fe11507b01db69
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-10-09 14:56:34 +0000

    versioning updates

--- future/web/core/constants.js
+++ future/web/core/constants.js
@@ -11,9 +11,9 @@ export const LOG_LEVELS = {
 };
 
 // Version constants for different modules
-export const BUILD_VERSION = '0.8.7-utilsRefactor';
-export const AUDIO_VERSION = '0.8.1-flow';
+export const BUILD_VERSION = '0.8.8-simplerCues';
+export const AUDIO_VERSION = '0.8.2-simplerCues';
 export const VIDEO_VERSION = '0.6-lucasKanadeFlow';
-export const UI_VERSION = '0.6.3-refinedDevPanel';
+export const UI_VERSION = '0.6.4-devHTML';
 export const LANGUAGES_VERSION = '0.2-spaEng';
-export const UTILS_VERSION = '0.9.5-cleanRefactor';
+export const UTILS_VERSION = '0.9.5-simplerRefactor';
