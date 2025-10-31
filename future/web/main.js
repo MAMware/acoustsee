8d5e29924771617d93ea6451917eff8fc608680a
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-10-31 19:26:29 +0000

    quick fix: missing import

--- future/web/main.js
+++ future/web/main.js
@@ -14,6 +14,7 @@ import { createEventBus } from './core/event-bus.js';
 import { initializeAnalytics } from './core/event-bus-analytics.js';
 import { settings } from './core/state.js';
 import { structuredLog, loggingConfig, initializeLogging } from './utils/logging.js';
+import { generateTraceId } from './utils/trace-id.js';
 import { 
   AccessibilityError, 
   showCriticalError, 
