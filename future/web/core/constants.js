166e97b30ca16b838fa8aa4afdb4b54d92e0026d
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-11-21 01:24:56 +0000

    fix: update build information with correct commit and timestamp in constants.js

--- future/web/core/constants.js
+++ future/web/core/constants.js
@@ -19,7 +19,7 @@ export const LANGUAGES_VERSION = '0.2-spaEng';
 export const UTILS_VERSION = '0.9.8-eventBus';
 
 // Build information (auto-injected by scripts/inject-build-info.js)
-export const BUILD_COMMIT = 'f0f84d5';
+export const BUILD_COMMIT = 'ea930c3';
 export const BUILD_BRANCH = 'v0.9.5-dynamicUI';
-export const BUILD_TIMESTAMP = '2025-11-20T23:56:00.545Z';
+export const BUILD_TIMESTAMP = '2025-11-21T01:24:42.275Z';
 
