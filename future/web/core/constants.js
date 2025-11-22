0ac48eee6c51a7c6850bc666bbfd8b48ca43b19e
Marcos Meneses <69035876+MAMware@users.noreply.github.com>
2025-11-22 00:57:02 +0000

    updated build info

--- future/web/core/constants.js
+++ future/web/core/constants.js
@@ -14,12 +14,12 @@ export const LOG_LEVELS = {
 export const BUILD_VERSION = '0.9.5';
 export const AUDIO_VERSION = '0.8.4-improvedUnlock';
 export const VIDEO_VERSION = '0.8.4-flowOrchestration';
-export const UI_VERSION = '0.8.4-missingTranslationDetections';
+export const UI_VERSION = '0.8.4-missingTranslationsGUI';
 export const LANGUAGES_VERSION = '0.4-enhancedI18N+';
 export const UTILS_VERSION = '0.9.8-eventBus';
 
 // Build information (auto-injected by scripts/inject-build-info.js)
-export const BUILD_COMMIT = 'f759639';
+export const BUILD_COMMIT = '3f37699';
 export const BUILD_BRANCH = 'v0.9.5-i18nAnalytics';
-export const BUILD_TIMESTAMP = '2025-11-21T23:36:07.642Z';
+export const BUILD_TIMESTAMP = '2025-11-22T00:56:09.023Z';
 
